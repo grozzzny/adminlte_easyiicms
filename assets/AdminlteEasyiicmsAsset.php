9cc170ee8ae16efbf5aa533f5f0b58d73ea34468
grozzzny <1234578>
2017-09-29 11:42:38 +0200

    adminlte_easyiicms

--- assets/AdminlteEasyiicmsAsset.php
+++ assets/AdminlteEasyiicmsAsset.php
@@ -8,12 +8,8 @@ class AdminlteEasyiicmsAsset extends \yii\web\AssetBundle
     public $css = [
         'adminlte_easyiicms.css',
     ];
-    public $js = [
-        'adminlte_easyiicms.js',
-    ];
     public $depends = [
         'grozzzny\depends\jquery_migrate\JqueryMigrateAsset',
-        'grozzzny\depends\waypoints\WaypointsAsset',
     ];
     public $jsOptions = array(
         'position' => \yii\web\View::POS_HEAD
